e1cd7ea6920969907ec7e772e4ab1a71238d8844
vineetsemwal <vineetsemwal82@gmail.com>
2024-02-26 16:55:25 +0530

    queue demo

--- javaexperiments/src/collectiondemos/ListEx3.java
+++ javaexperiments/src/collectiondemos/ListEx3.java
@@ -1,16 +1,29 @@
 package collectiondemos;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class ListEx3 {
     public static void main(String[] args) {
-        List<Product>list=new LinkedList<>();
-        list.add(new Product(1, "imac", 80000));
-        list.add(new Product(2, "iphone-12", 50000));
-        list.add(new Product(3, "iphone-13", 60000));
-        list.add(new Product(4, "htc", 30000));
+    //  experiemnt with implementations linked LinkedList,
+          Deque<Product> queue = new LinkedBlockingDeque<>(1);
+        // add at the start, if queue is full,then it  throws  IllegalStateException
+        queue.addLast(new Product(1, "imac", 80000));
+        // add at the start, if queue is full,then it  throws  IllegalStateException
+//        queue.addFirst(new Product(2, "iphone-12", 50000));
+        // add at the start, if queue is full,then it doesn't throw exception
+        queue.offerFirst(new Product(3, "galaxy-g1", 40000));
+
+        // fetch first element of Q
+        Product first1 = queue.peekFirst();
+
+        // fetch first element of Q and element will be removed from the Queue, if Q is empty null is returned
+        Product first2 = queue.pollFirst();
+
+        // fetches first element , if Q is empty, throws NoSuchElementException
+        Product first3=queue.getFirst();
+
 
     }
 }
