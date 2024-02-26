56b561552ffa7f491bf90323dce5d9731725fd36
vineetsemwal <vineetsemwal82@gmail.com>
2024-02-26 12:59:17 +0530

    Map Demo

---
+++ javaexperiments/src/collectiondemos/MapEx.java
@@ -0,0 +1,36 @@
+package collectiondemos;
+
+import java.util.*;
+
+public class MapEx {
+    public static void main(String[] args) {
+        Map<Integer,Product>map=new LinkedHashMap<>();
+        map.put(2,new Product(2,"moto",30000));
+        map.put(1,new Product(1,"samsung",20000));
+        map.put(3,new Product(3,"iphone10",50000));
+        map.put(3,new Product(3,"iphone12",60000));
+        System.out.println("map size="+map.size());
+        System.out.println(map);
+        Set<Integer>keys=map.keySet();
+
+        System.out.println("****iterating on keys and fetching values by keys");
+        for (Integer key:keys){
+          Product value  =map.get(key);
+            System.out.println(value);
+        }
+        System.out.println("*****iterating on values");
+        Collection<Product> values=map.values();
+        for (Product value:values){
+            System.out.println(value);
+        }
+
+        System.out.println("****iterating on entries");
+      Set<Map.Entry<Integer,Product>>entries   =map.entrySet();
+        for (Map.Entry<Integer,Product> iterated:entries){
+          Integer  key  = iterated.getKey();
+          Product product=iterated.getValue();
+            System.out.println("key="+key);
+            System.out.println("product="+product);
+        }
+    }
+}
